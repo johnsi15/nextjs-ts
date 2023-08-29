5778d6b77646538413cbc4b0599c3594c9f6ccb6
John Serrano <andrey_201015@hotmail.com>
2023-08-29 15:04:13 -0500

    fix problem with src img, client/server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,40 @@
 // import Image from 'next/image'
 'use client'
-import {useState} from 'react'
-import {RandomFox} from '../components/RandomFox'
-
-const randomNumber = () => Math.floor(Math.random() * 123) + 1
-const generateId = () => Math.random().toString(36).substring(2, 15);
+import { useEffect, useState } from 'react'
+import { RandomFox } from '../components/RandomFox'
 
 type ImageItem = { id: string; image_url: string }
 
 export default function Home() {
+  const [images, setImages] = useState<ImageItem[]>([])
 
-  const [images, setImages] = useState<ImageItem[]>([
-    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
-    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
-    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
-    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`}
-  ])
+  useEffect(() => {
+    const randomNumber = () => Math.floor(Math.random() * 123) + 1
+    const generateId = () => Math.random().toString(36).substring(2, 15)
+    setImages([
+      {
+        id: generateId(),
+        image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+      },
+      {
+        id: generateId(),
+        image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+      },
+      {
+        id: generateId(),
+        image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+      },
+      {
+        id: generateId(),
+        image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`,
+      },
+    ])
+  }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-3">
+    <main className='flex min-h-screen flex-col items-center justify-center p-24 gap-3'>
       <h1 className='text-5xl font-medium'>Hello world</h1>
-      {images.map(({ id, image_url}) => (
+      {images.map(({ id, image_url }) => (
         <div key={id} className='p-4'>
           <RandomFox image={image_url} alt='fox' />
         </div>
