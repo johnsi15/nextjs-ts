3b11e8890c6240d29c92bf0e25dc0f7ed20ff573
John Serrano <andrey_201015@hotmail.com>
2023-08-29 14:54:12 -0500

    improving types images urls with useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,25 @@ import {useState} from 'react'
 import {RandomFox} from '../components/RandomFox'
 
 const randomNumber = () => Math.floor(Math.random() * 123) + 1
+const generateId = () => Math.random().toString(36).substring(2, 15);
+
+type ImageItem = { id: string; image_url: string }
 
 export default function Home() {
 
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`,
-    `https://randomfox.ca/images/${randomNumber()}.jpg`
+  const [images, setImages] = useState<ImageItem[]>([
+    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
+    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
+    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`},
+    {id: generateId(), image_url: `https://randomfox.ca/images/${randomNumber()}.jpg`}
   ])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-3">
       <h1 className='text-5xl font-medium'>Hello world</h1>
-      {images.map((image, index) => (
-        <div key={index} className='p-4'>
-          <RandomFox image={image} alt='fox' />
+      {images.map(({ id, image_url}) => (
+        <div key={id} className='p-4'>
+          <RandomFox image={image_url} alt='fox' />
         </div>
       ))}
     </main>
