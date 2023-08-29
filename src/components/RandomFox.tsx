340bd84ce613258106a7597389a53deab496d231
John Serrano <andrey_201015@hotmail.com>
2023-08-29 12:09:46 -0500

    Add my first component with react tsx

---
+++ src/components/RandomFox.tsx
@@ -0,0 +1,18 @@
+// type ImageProps = {
+//   image: string
+//   alt: string
+// }
+
+// También se puede hacer así:
+interface ImageProps {
+  image: string
+  alt: string
+}
+
+export const RandomFox = ({ image, alt }: ImageProps): JSX.Element => {
+  return (
+    <div>
+      <img width={320} height='auto' src={image} alt={alt} className="rounded" />
+    </div>
+  )
+}
