9f73e37310a06a3357d784a2fd4de38f41f34368
John Serrano <andrey_201015@hotmail.com>
2023-08-30 15:38:00 -0500

    Add lazy loading with observables

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -3,7 +3,7 @@
 //   alt: string
 // }
 
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 // También se puede hacer así:
 interface ImageProps {
@@ -13,10 +13,29 @@ interface ImageProps {
 
 export const RandomFox = ({ image, alt }: ImageProps): JSX.Element => {
   const node = useRef<HTMLImageElement>(null)
+  const [src, setSrc] = useState('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=')
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      console.log({ entries }) // Array of 1 element img
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // console.log('visible')
+          setSrc(image)
+        }
+      })
+    })
+
+    if (node.current != null) {
+      observer.observe(node.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [image])
 
   return (
-    <div>
-      <img ref={node} width={320} height='auto' src={image} alt={alt} className='rounded' />
-    </div>
+    <img ref={node} width={320} height='auto' src={src} alt={alt} className='rounded-md bg-gray-400' />
   )
 }
